74eedfaab07e8ed9996981566fa03e69bccc05ea
Fatih İnci <fatih969692@gmail.com>
2022-12-03 18:24:09 +0300

    first commit

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from "ui";
+import { Button, InputText } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Button />
+      <InputText />
     </div>
   );
 }
